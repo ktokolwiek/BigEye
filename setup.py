714f7ff4d9ba61036ea94e30d05255f339c4bf9c
Hugggsy <hugo.depretz@gmail.com>
2018-08-02 18:25:57 +0100

    set up file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='bigeye',
+      version='0.1',
+      description='Monitoring data quality across systems',
+      url='https://github.com/BBOXX/BigEye.git',
+      author='Hugggsy',
+      license='Apache',
+      packages=['bigeye'],
+      install_requires=[
+          'psycopg2',
+          'datadog',
+          'ruamel.yaml',
+          'boto3'
+      ],
+      zip_safe=False)
